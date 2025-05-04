84d08921d108c2a521de366e205f7d889bc70dc9
Landon Carderara <ltcarderara@gmail.com>
2025-05-04 16:33:44 -0500

    QTE
    Created the script for the QTE and made sure it functioned

---
+++ Assets/Scripts/QTEsys.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class QTEsys : MonoBehaviour
+{
+    public GameObject Displaybox;
+    public GameObject Passbox;
+    public int QTEGen;
+    public int WaitingForKey;
+    public int CorrectKey;
+    public int CountingDown;
+
+    void Update ()
+    {
+        if (WaitingForKey == 0)
+        {
+            QTEGen = Random.Range(1,4);
+            CountingDown = 1;
+            StartCoroutine(CountDown());
+            if (QTEGen == 1)
+            {
+                WaitingForKey = 1;
+                Displaybox.GetComponent<Text>().text = "[E]";
+            }
+            if (QTEGen == 2)
+            {
+                WaitingForKey = 1;
+                Displaybox.GetComponent<Text>().text = "[R]";
+            }
+            if (QTEGen == 3)
+            {
+                WaitingForKey = 1;
+                Displaybox.GetComponent<Text>().text = "[T]";
+            }
+            
+        }
+        if (QTEGen == 1)
+        {
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    CorrectKey = 1;
+                    StartCoroutine(KeyPressing());
+                }
+                else
+                {
+                    CorrectKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+        if (QTEGen == 2)
+        {
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    CorrectKey = 1;
+                    StartCoroutine(KeyPressing());
+                }
+                else
+                {
+                    CorrectKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+        if (QTEGen == 3)
+        {
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    CorrectKey = 1;
+                    StartCoroutine(KeyPressing());
+                }
+                else
+                {
+                    CorrectKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+
+    }
+    IEnumerator KeyPressing() 
+    {
+        QTEGen = 4;
+        if (CorrectKey == 1)
+        {
+            CountingDown = 2;
+            Passbox.GetComponent<Text>().text = "Success!";
+            yield return new WaitForSeconds(1.5f);
+            CorrectKey = 0;
+            Passbox.GetComponent<Text>().text = " ";
+            Displaybox.GetComponent<Text>().text = " ";
+            yield return new WaitForSeconds(1.5f);
+            WaitingForKey = 0;
+            CountingDown = 1;
+        }
+        if (CorrectKey == 2)
+        {
+            CountingDown = 2;
+            Passbox.GetComponent<Text>().text = "Fail!";
+            yield return new WaitForSeconds(1.5f);
+            CorrectKey = 0;
+            Passbox.GetComponent<Text>().text = " ";
+            Displaybox.GetComponent<Text>().text = " ";
+            yield return new WaitForSeconds(1.5f);
+            WaitingForKey = 0;
+            CountingDown = 1;
+        }
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(3.5f);
+        if (CountingDown == 1)
+        {
+            QTEGen = 4;
+            CountingDown = 2;
+            Passbox.GetComponent<Text>().text = "Fail!";
+            yield return new WaitForSeconds(1.5f);
+            CorrectKey = 0;
+            Passbox.GetComponent<Text>().text = " ";
+            Displaybox.GetComponent<Text>().text = " ";
+            yield return new WaitForSeconds(1.5f);
+            WaitingForKey = 0;
+            CountingDown = 1;
+        }
+    }
+
+    
+}
