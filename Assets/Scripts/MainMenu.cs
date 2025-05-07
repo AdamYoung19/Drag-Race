2029b060729a1758910d666b0a98c7fc36934213
LawsonJLerille <ljl035@email.latech.edu>
2025-05-06 22:12:57 -0500

    MUSIC
    Added Music with Options Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
 }
